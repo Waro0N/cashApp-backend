a803b5fd9a09126207034009943136163983fe03
Divyom Kumar <divyom.kumar@unilever.com>
2023-04-02 16:24:51 +0530

    Added a query in Dashboard API to get user Specific data

--- cashflow/views.py
+++ cashflow/views.py
@@ -22,3 +22,11 @@ class CreateCashFlow(ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save()
+
+    def get_queryset(self):
+        if "all" in self.request.GET:
+            queryset=CashFlow.objects.all()
+        if "created_by" in self.request.GET:
+            created_by = self.request.GET['created_by']
+            queryset=CashFlow.objects.filter(created_by=created_by)
+        return queryset
