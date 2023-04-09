24e32ec34afbb4df43cd0edf747fcb0e7072b813
Divyom Kumar <divyom.kumar@unilever.com>
2023-04-10 00:20:49 +0530

    Changed calender API logic | Still in progress

--- calenderdetails/views.py
+++ calenderdetails/views.py
@@ -13,30 +13,43 @@ class CalenderDetails(ListAPIView):
 
     def get(self, request, *args, **kargs):
         queryset = CashFlow.objects.all()
-        
+        events=[]
         if 'created_by' in self.request.GET:
             created_by = self.request.GET['created_by']
             queryset=queryset.filter(created_by=created_by)
-        serializer = self.get_serializer(queryset, many=True)
-        data=serializer.data
-        comp_data={}
-        credit_data={}
-        for i in data:
-            date = i['date']
-            debit_item= i['debit']
-            credit_item = i['credit']
-            if date in comp_data:
-                comp_data[date] += debit_item
-            else:
-                comp_data[date] = debit_item
-        
-            if debit_item == 0:
-                if date in credit_data:
-                    credit_data[date] += credit_item
-                else:
-                    credit_data[date] = credit_item
-        print(comp_data)
-        print(credit_data)
-        results = [{'start': cal_date , 'title': 'Debit: ' + str(total)} for cal_date, total in comp_data.items() ]
 
-        return Response(results)
+            for i in queryset:
+                if i.debit > 0:
+                    title = f"Debit: {i.debit}"
+                if i.debit ==0:
+                    title = f"Credit: {i.credit}"
+                event = {
+                    "start": i.date.strftime('%Y-%m-%d'),
+                    "title" : title
+                }
+                events.append(event)
+
+
+        # serializer = self.get_serializer(queryset, many=True)
+        # data=serializer.data
+        # comp_data={}
+        # credit_data={}
+        # for i in data:
+        #     date = i['date']
+        #     debit_item= i['debit']
+        #     credit_item = i['credit']
+        #     if date in comp_data:
+        #         comp_data[date] += debit_item
+        #     else:
+        #         comp_data[date] = debit_item
+        
+        #     if debit_item == 0:
+        #         if date in credit_data:
+        #             credit_data[date] += credit_item
+        #         else:
+        #             credit_data[date] = credit_item
+        # print(comp_data)
+        # print(credit_data)
+        # results = [{'start': cal_date , 'title': 'Debit: ' + str(total)} for cal_date, total in comp_data.items() ]
+
+        return Response(events)
